4e775a9e8f21e1d42dc352ebb69a9c8d78a5a5de
Sujin Lee <soolovepat@naver.com>
2023-06-30 10:18:00 +0900

    Implement Create React-Router-Dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,11 @@
 import "./App.css";
-import React, { useState } from "react";
-import Todohead from "./components/Todohead";
-import TodoCreate from "./components/TodoCreate";
-import TodoList from "./components/TodoList";
+import React from "react";
+import Router from "./shared/Router";
 
 function App() {
-  const [todos, setTodos] = useState([
-    {
-      id: 1,
-      title: "React Todo-List 반복 학습하기",
-      desc: "10번 반복 !!!",
-      isDone: false,
-    },
-    {
-      id: 2,
-      title: "Redux 학습하기",
-      desc: "예제 10번 따라하기 !",
-      isDone: false,
-    },
-  ]);
-
   return (
     <main>
-      <Todohead />
-      <TodoCreate todos={todos} setTodos={setTodos} />
-      <TodoList todos={todos} setTodos={setTodos} />
+      <Router />
     </main>
   );
 }
---
+++ src/shared/Router.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "../pages/Home";
+import DetailTodo from "../pages/DetailTodo";
+
+function Router() {
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/DetailTodo" element={<DetailTodo />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default Router;
