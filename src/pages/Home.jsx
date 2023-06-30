7d2083ea7a630cab45193590678684fd2e6d8ee0
Sujin Lee <soolovepat@naver.com>
2023-06-30 19:58:02 +0900

    Remove unused item

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,20 +4,6 @@ import TodoCreate from "./components/TodoCreate";
 import TodoList from "./components/TodoList";
 
 function Home() {
-  // const [todos, setTodos] = useState([
-  //   {
-  //     id: 1,
-  //     title: "React Todo-List 반복 학습하기",
-  //     desc: "10번 반복 !!!",
-  //     isDone: false,
-  //   },
-  //   {
-  //     id: 2,
-  //     title: "Redux 학습하기",
-  //     desc: "예제 10번 따라하기 !",
-  //     isDone: false,
-  //   },
-  // ]);
   return (
     <div>
       <Todohead />
