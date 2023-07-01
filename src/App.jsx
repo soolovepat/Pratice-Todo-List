66da56b276a5e9d29752461859edb4252e31487c
Sujin Lee <soolovepat@naver.com>
2023-07-01 17:07:35 +0900

    Add GlobalStyle

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
 import React from "react";
 import Router from "./shared/Router";
+import GlobalStyle from "./GlobalStyle";
 
 function App() {
   return (
     <main>
+      <GlobalStyle />
       <Router />
     </main>
   );
---
+++ src/GlobalStyle.jsx
@@ -0,0 +1,11 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    font-family: 'Noto Sans KR', sans-serif;
+    margin: 30px;
+    
+  }
+`;
+
+export default GlobalStyle;
